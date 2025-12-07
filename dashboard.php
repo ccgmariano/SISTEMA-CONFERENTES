92812ddcd368321df90018104fc6d3b606323544
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-07 14:45:55 -0300

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config.php';
-
-// Garante que apenas usuários logados acessem
-require_login();
-
-include __DIR__ . '/app/views/header.php';
-include __DIR__ . '/app/views/dashboard.php';
-include __DIR__ . '/app/views/footer.php';
