6e673294c7391b38ea6ae9b50aafe2927bbbd899
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-08 15:32:24 -0300

    Update database.php

--- app/database.php
+++ app/database.php
@@ -6,7 +6,17 @@ class Database {
 
     public static function connect() {
         if (!self::$db) {
-            $path = __DIR__ . '/../database.sqlite';
+
+            // Diretório de escrita permitido no Render
+            $dir = '/var/data';
+
+            // Criar diretório caso não exista
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true);
+            }
+
+            $path = $dir . '/database.sqlite';
+
             self::$db = new PDO("sqlite:" . $path);
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
