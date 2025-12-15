9c59dfb0f48898adef5dd90b09f1528e2c64a9fc
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-15 18:38:11 -0300

    Update test_write.php

--- test_write.php
+++ test_write.php
@@ -1,5 +1,10 @@
 <?php
 $path = '/var/data/db/teste.txt';
 
-file_put_contents($path, 'ok');
-echo 'gravou';
+$result = @file_put_contents($path, 'ok');
+
+if ($result === false) {
+    echo 'FALHOU';
+} else {
+    echo 'GRAVOU';
+}
