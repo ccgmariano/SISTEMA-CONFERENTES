70ebf97522ca6209f6501a3b8ccd20a3ca83e9de
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 10:49:19 -0300

    Create decks.php

---
+++ app/crud/decks.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'tabela' => 'decks',
+    'titulo' => 'Decks',
+    'campos' => [
+        'nome' => [
+            'label' => 'Nome',
+            'type' => 'text',
+            'required' => true
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
