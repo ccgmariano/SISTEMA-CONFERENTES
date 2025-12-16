e22df2706c1934f335a00e4fb8ecf419b583b2cd
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 09:35:11 -0300

    Create navios.php

---
+++ app/crud/navios.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'tabela' => 'navios',
+    'titulo' => 'Navios',
+    'campos' => [
+        'nome' => [
+            'label' => 'Nome',
+            'type' => 'text',
+            'required' => true
+        ],
+        'imo' => [
+            'label' => 'IMO',
+            'type' => 'text'
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
