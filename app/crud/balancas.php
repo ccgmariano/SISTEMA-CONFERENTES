ea6344cb30fdd972156550fc64191696321286b8
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 11:24:38 -0300

    Create balancas.php

---
+++ app/crud/balancas.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'tabela' => 'balancas',
+    'titulo' => 'Balanças',
+    'campos' => [
+        'nome' => [
+            'label' => 'Nome',
+            'type' => 'text',
+            'required' => true
+        ],
+        'identificador' => [
+            'label' => 'Identificador',
+            'type' => 'text'
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
