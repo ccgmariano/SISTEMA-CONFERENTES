a63a3d90da96812dbe250917554e6ad809530084
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 10:24:33 -0300

    Create cargas.php

---
+++ app/crud/cargas.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'tabela' => 'cargas',
+    'titulo' => 'Cargas',
+    'campos' => [
+        'nome' => [
+            'label' => 'Nome',
+            'type' => 'text',
+            'required' => true
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
