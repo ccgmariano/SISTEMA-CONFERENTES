6f2c1d80a1ef5f0afce83b952a037ce244fcd10b
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 10:34:17 -0300

    Create equipamentos.php

---
+++ app/crud/equipamentos.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'tabela' => 'equipamentos',
+    'titulo' => 'Equipamentos',
+    'campos' => [
+        'nome' => [
+            'label' => 'Nome',
+            'type' => 'text',
+            'required' => true
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
