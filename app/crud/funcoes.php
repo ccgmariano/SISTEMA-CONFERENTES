5cd2afb474433823b5aa58a28c92a24b87a9d09d
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-16 12:55:15 -0300

    Create funcoes.php

---
+++ app/crud/funcoes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'tabela' => 'funcoes',
+    'titulo' => 'Funções',
+    'campos' => [
+        'nome' => [
+            'label' => 'Função',
+            'type' => 'text',
+            'required' => true
+        ],
+        'ativo' => [
+            'label' => 'Ativo',
+            'type' => 'text'
+        ]
+    ]
+];
