b3c0c157ce0513695bdecd883c6be32844466b38
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-17 10:57:20 -0300

    Update atn.php

--- atn.php
+++ atn.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/app/database.php';
 
 $db = Database::connect();
 
-$db->exec("ALTER TABLE navios ADD COLUMN num_poroes INTEGER");
-$db->exec("ALTER TABLE navios ADD COLUMN ativo INTEGER DEFAULT 1");
+$db->exec("ALTER TABLE navios ADD COLUMN decks TEXT");
 
 echo "OK";
