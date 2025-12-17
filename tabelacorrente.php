c99eed7d50303b37152f366fc95e9e6674d969f0
ccgmariano <134815442+ccgmariano@users.noreply.github.com>
2025-12-17 11:07:23 -0300

    Update tabelacorrente.php

--- tabelacorrente.php
+++ tabelacorrente.php
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS periodo_funcoes (
+CREATE TABLE IF NOT EXISTS periodo_conferentes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    periodo_id INTEGER NOT NULL,
-    funcao_id INTEGER NOT NULL
+    periodo_funcao_id INTEGER NOT NULL,
+    associado_id INTEGER NOT NULL
 );
